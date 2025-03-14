24d024981ac6328961b6a61b6f1820d52522f8ff
bao1405 <lqbbiker15052@gmail.com>
2025-03-14 15:49:07 +0700

    update

--- ss10/src/bai9/Developer.java
+++ ss10/src/bai9/Developer.java
@@ -4,18 +4,11 @@ public class Developer extends Employee {
     private String programmingLanguage;
 
     public Developer() {
-        super();
-        this.programmingLanguage = "Unknown";
     }
 
     public Developer(String name, String id, double salary, String programmingLanguage) {
         super(name, id, salary);
         this.programmingLanguage = programmingLanguage;
-    }
-
-    @Override
-    public double getSalary() {
-        return salary;
     }
 
     @Override
